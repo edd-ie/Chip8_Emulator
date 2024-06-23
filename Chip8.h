a95b71b221e10de7f02005af0a8b35ad7492ffa2
edd-ie <ogendoeddie@gmail.com>
2024-06-23 18:51:02 -0300

    Fonts

--- Chip8.h
+++ Chip8.h
@@ -5,10 +5,39 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 #include <cstdint>
+#include <fstream>
 
 class Chip8
 {
     const unsigned int START_ADDRESS = 0x200;
+    const unsigned int FONTSET_SIZE = 80;
+
+    void LoadROM(char const* filename)
+    {
+        // Open the file as a stream of binary and move the file pointer to the end
+        std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+        if (file.is_open())
+        {
+            // Get size of file and allocate a buffer to hold the contents
+            std::streampos size = file.tellg();
+            char* buffer = new char[size];
+
+            // Go back to the beginning of the file and fill the buffer
+            file.seekg(0, std::ios::beg);
+            file.read(buffer, size);
+            file.close();
+
+            // Load the ROM contents into the Chip8's memory, starting at 0x200
+            for (long i = 0; i < size; ++i)
+            {
+                memory[START_ADDRESS + i] = buffer[i];
+            }
+
+            // Free the buffer
+            delete[] buffer;
+        }
+    }
 
 public:
     uint8_t registers[16]{};
@@ -22,6 +51,31 @@ public:
     uint8_t keypad[16]{};
     uint32_t video[64 * 32]{};
     uint16_t opcode{};
+    uint8_t fontsetfontset[80] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
+    Chip8()
+    {
+        //Initialize the program counter
+        pc=START_ADDRESS;
+
+    }
 
 };
 

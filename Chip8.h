aa3e413783c30c1960dd69dbe191a4e0dddf564a
edd-ie <ogendoeddie@gmail.com>
2024-06-24 02:41:34 -0300

    Fonts

--- Chip8.h
+++ Chip8.h
@@ -58,33 +58,6 @@ class Chip8
         }
     }
 
-    void LoadROM(char const* filename)
-    {
-        // Open the file as a stream of binary and move the file pointer to the end
-        std::ifstream file(filename, std::ios::binary | std::ios::ate);
-
-        if (file.is_open())
-        {
-            // Get size of file and allocate a buffer to hold the contents
-            std::streampos size = file.tellg();
-            char* buffer = new char[size];
-
-            // Go back to the beginning of the file and fill the buffer
-            file.seekg(0, std::ios::beg);
-            file.read(buffer, size);
-            file.close();
-
-            // Load the ROM contents into the Chip8's memory, starting at 0x200
-            for (long i = 0; i < size; ++i)
-            {
-                memory[START_ADDRESS + i] = buffer[i];
-            }
-
-            // Free the buffer
-            delete[] buffer;
-        }
-    }
-
 public:
     uint8_t registers[16]{};
     uint8_t memory[4096]{};

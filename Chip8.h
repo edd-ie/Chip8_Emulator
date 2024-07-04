76bd67bf7d02a5cdd81370b85bea53c8bace6ef5
edd-ie <ogendoeddie@gmail.com>
2024-07-04 00:45:23 -0300

    Video display instruction set

--- Chip8.h
+++ Chip8.h
@@ -361,6 +361,74 @@ public:
         const uint16_t address = opcode & 0x0FFFu;
         index = address;
     }
+
+    /**
+    * Jump to register 0 + nnn
+    */
+    void OP_Bnnn()
+    {
+        const uint16_t address = opcode & 0x0FFFu;
+        pc = registers[0] + address;
+
+    }
+
+    /**
+    * set Vx = random byte AND kk
+    */
+    void OP_Cxkk()
+    {
+
+        const uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        const uint8_t byte = opcode & 0x00FFu;
+        registers[Vx] = randByte(randGen) & byte;
+    }
+
+    /**
+    * Iterate over the sprite, row by row and column by column.
+    * 8 columns because sprite is 8px wide.
+    * If a sprite pixel is ON then there may be a collision with what’s already being displayed
+    * Check if our screen pixel in the same location is set.
+    * If so we must set the VF register to express collision.
+    * XOR the screen pixel with 0xFFFFFFFF to essentially XOR it with the sprite pixel
+    */
+    void OP_Dxyn()
+    {
+        const uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        const uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+        const uint8_t height = opcode & 0x000Fu;
+
+        // Wrap if going beyond screen boundaries
+        const uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+        const uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+        registers[0xF] = 0;
+
+        for (unsigned int row = 0; row < height; ++row)
+        {
+            const uint8_t spriteByte = memory[index + row];
+
+            for (unsigned int col = 0; col < 8; ++col)
+            {
+                const uint8_t spritePixel = spriteByte & (0x80u >> col);
+                const uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+                // Sprite pixel is on
+                if (spritePixel)
+                {
+                    // Screen pixel also on - collision
+                    if (*screenPixel == 0xFFFFFFFF)
+                    {
+                        registers[0xF] = 1;
+                    }
+
+                    // Effectively XOR with the sprite pixel
+                    *screenPixel ^= 0xFFFFFFFF;
+                }
+            }
+        }
+    }
+
+
 };
 
 
